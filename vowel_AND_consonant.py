10e56fbdd0b8c6b96af8784d281f41e02d313843
Rahul Jadhav <138716867+Rahul7434@users.noreply.github.com>
2023-07-31 01:50:02 +0530

    Add files via upload

---
+++ vowel_AND_consonant.py
@@ -0,0 +1,10 @@
+ch=input("Enter any Character and check it's vowel or Consonant")
+
+if ch=='a' or ch=='e' or ch=='i'or ch=='o' or ch=='u':
+    print(f"{ch} it is a vowel")
+elif ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+    print(f"{ch} it is a vowel")
+else:
+    print(f"{ch} it is a consonant")
+
+
